13d5d7024251cedf1cc107a5684adb3f97dac530
Cole Edwards <ColeEdwards320@gmail.com>
2025-06-16 10:24:29 -0500

    Algo Update
    Updated Solving Algorithm - Rewrote the algorithm to finding the incorrect answers and then find the potential answers for the curSq

--- BruteForce.py
+++ BruteForce.py
@@ -45,20 +45,47 @@ def sudChk(board, curCub, pos):
 def noNumDict(board, solvSq):
     noNoNums = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
     for pos in range(0,9):
+        print("---------------CURRENT POSITION: ", pos, " ------------------------")
         if(solvSq[pos] == 0):
             for i in range(0,9):
+                print("CURRENT pos = ", pos, "pos//3 = ", pos//3, " (pos+1)//3 = ", (pos+1)//3, "AND CURRENT i = ", i)
+                #check the 3x3 for numbers
                 print("checking solvSq[i] = ", solvSq[i])
                 if(solvSq[i] != 0):
                     print("adding: ", solvSq[i])
                     noNoNums[pos].append(solvSq[i])
-                print("checking board[pos][i] = ", solvSq[i])
-                if(board[pos][i] != 0):
-                    if(board[pos][i]):
-                    print("adding: ", board[pos][i])
-                    noNoNums[pos].append(board[pos][i])
+                #check across the current array our pos is in
+                #do integer division to keep the correct array
+                print("checking board[pos//3][i] = ", board[pos//3][i])
+                if(board[pos//3][i] != 0 and board[pos//3][i] not in noNoNums[pos]):
+                    print("adding: ", board[pos//3][i])
+                    noNoNums[pos].append(board[pos//3][i])
+                #keep the current pos and iterate down the arrays
+                if(pos in [0,3,6]):
+                    newpos = 0
+                elif(pos in [1,4,7]):
+                    newpos = 1
+                elif(pos in [2,5,8]):
+                    newpos = 2
+                print("checking board[i][pos] = ", board[i][newpos])
+                if(board[i][newpos] != 0 and board[i][newpos] not in noNoNums[pos]):
+                    print("adding: ", board[i][newpos])
+                    noNoNums[pos].append(board[i][newpos])
     print("cur dictionary of numbers that cannot be")
     print(noNoNums)
+    return noNoNums
 
+#---------------------------------------------------------------------------------------------------------
+#use the dictionary to find the number that should go in place
+#find potential answers from the numbers that it can't be
+def potAns(badNums):
+    potNums = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
+    for i in range(0,9):
+        for j in range(1,10):
+            if(j not in badNums[i] and badNums[i] != []):
+                potNums[i].append(j)         
+    print("potential answers")
+    print(potNums)
 
 #---------------------------------------------------------------------------------------------------------    
 #solve the 3x3 
@@ -75,7 +102,13 @@ def sudSolv(board, curSq):
         for k in range(0,3):
             solvSeq.append(board[j][k])
     print("cur solvSeq", solvSeq)
-    noNumDict(board, solvSeq)
+    badnums = noNumDict(board, solvSeq)
+    potAns(badnums)
+    #increment square
+    curSq += 1
+    #return solved board
+    #UPDATE board TO SOLVED BOARD VARIABLE
+    return board
     '''compareSeq = [1,2,3,4,5,6,7,8,9]
     #compare the spots for 
     ordSeq = solvSeq.copy()
@@ -117,3 +150,19 @@ boardSolved = sudSolv(SodBoard, 0)
 print("Solved Board:")
 for row in boardSolved:
     print(row)
+
+
+
+
+#Algorithms
+#Knuth's exact problem algorithm
+#interesting read
+#https://11011110.github.io/blog/2008/01/10/analyzing-algorithm-x.html
+
+#Backtracking - depth first search
+#simpler to understand however Big O notation is much higher
+#Goal of the algo:
+#fill in the first blank space, then slowly work through the next blank spaces until you hit one where its unsolveable.
+#if an unsolveable spot is met, backtrack to a previous point and try to change that, hoping it changes the outcome.
+#BackTracking uses set rules to contain the seach, this case it will be that each row, column, and 3x3 square must have 1-9. no repeats.
+#
