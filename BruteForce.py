21fdb3dfadc8b2949fcca09947e222bcc5c0baec
Cole Edwards <ColeEdwards320@gmail.com>
2025-06-23 09:08:00 -0500

    Refactor
    Refactoring the algo in which numsthat cannot be are found

--- BruteForce.py
+++ BruteForce.py
@@ -40,72 +40,130 @@ def sudChk(board, curCub, pos):
     print("else hit")
     return (True, curCub)
 #---------------------------------------------------------------------------------------------------------
-#Create a dictionary that can store all numbers that the cur pos CANNOT BE
+#find the square that our current item is apart of
+def getSqNum(cellRow, cellCol):
+    #use the row and column
+    squareRow = (cellRow - 1) / 3
+    squareCol = (cellCol - 1) / 3
+    squareNum = int((squareRow * 3) + squareCol + 1)
+    print("the square we are looking for is: ", squareNum)
+    if(squareNum > 8):
+        return 8
+    else:
+        return squareNum
 
-def noNumDict(board, solvSq):
-    noNoNums = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
-    for pos in range(0,9):
-        print("---------------CURRENT POSITION: ", pos, " ------------------------")
-        if(solvSq[pos] == 0):
-            for i in range(0,9):
-                print("CURRENT pos = ", pos, "pos//3 = ", pos//3, " (pos+1)//3 = ", (pos+1)//3, "AND CURRENT i = ", i)
-                #check the 3x3 for numbers
-                print("checking solvSq[i] = ", solvSq[i])
-                if(solvSq[i] != 0):
-                    print("adding: ", solvSq[i])
-                    noNoNums[pos].append(solvSq[i])
-                #check across the current array our pos is in
-                #do integer division to keep the correct array
-                print("checking board[pos//3][i] = ", board[pos//3][i])
-                if(board[pos//3][i] != 0 and board[pos//3][i] not in noNoNums[pos]):
-                    print("adding: ", board[pos//3][i])
-                    noNoNums[pos].append(board[pos//3][i])
-                #keep the current pos and iterate down the arrays
-                if(pos in [0,3,6]):
-                    newpos = 0
-                elif(pos in [1,4,7]):
-                    newpos = 1
-                elif(pos in [2,5,8]):
-                    newpos = 2
-                print("checking board[i][pos] = ", board[i][newpos])
-                if(board[i][newpos] != 0 and board[i][newpos] not in noNoNums[pos]):
-                    print("adding: ", board[i][newpos])
-                    noNoNums[pos].append(board[i][newpos])
-    print("cur dictionary of numbers that cannot be")
-    print(noNoNums)
-    return noNoNums
+#get a list of all 3x3 squares for later use
+def getSq(board):
+    cubSeq = {}
+    k = 0
+    for r in range(0,3):
+        for c in range(0,3):
+            block = []
+            for i in range(0,3):
+                for j in range(0,3):
+                    block.append(board[3*r + i][3*c + j])
+            if(k not in cubSeq):
+                cubSeq[k] = [block]
+                k+=1
+            else:
+                cubSeq[k].append(block) 
+                k+=1
+    return cubSeq
+
+#---------------------------------------------------------------------------------------------------------
+#Create a dictionary that can store all numbers that the cur pos CANNOT BE
+def noNumDict(board):
+    #create a dict to store all the bad numbers of the array
+    noNoNums = {}
+
+    #for curSeq in board:
+    for curArr in range(0,9):
+        print("==========================curArr:", curArr, "=============================")
+        curSeq = board[curArr]
+        print("current Sequence: ", curSeq)
+        #check along the horizontal axis 
+        # in the array
+        #we use range() because we want i to also be used to name keys for board positions
+        for i in range(0,9):
+            if(curSeq[i] == 0):
+                for j in range(0,9):
+                    if(curSeq[j] != 0):
+                        if(curArr == 0 and i not in noNoNums):
+                            noNoNums[i] = [curSeq[j]]
+                        elif(curArr > 0 and (i + (9*curArr)) not in noNoNums):
+                            noNoNums[(i + (9*curArr))] = [curSeq[j]]
+                        else:
+                            noNoNums[(i + (9*curArr))].append(curSeq[j]) 
+
+        #check along the Vertical axis
+        # keep the position but iterate through all arrays in the board
+        
+        for i in range(0,9):
+            if(curSeq[i] == 0):
+                for j in range(0,9):
+                    if(board[j][i] != 0):
+                        if(curArr == 0 and i not in noNoNums):
+                            noNoNums[i] = [curSeq[j]]
+                        elif(curArr > 0 and (i + (9*curArr)) not in noNoNums):
+                            noNoNums[(i + (9*curArr))] = [board[j][i]]
+                        elif(board[j][i] not in noNoNums[(i + (9*curArr))]):
+                            noNoNums[(i + (9*curArr))].append(board[j][i])
+        
+        #check within the 3x3 square the curpos belongs too
+        #find the square its apart of
+
+        #call getSq to get a dictionary of all 3x3's
+        curSqs = getSq(board)
+        print("curSqs = ", curSqs)
+        #then call a function to find the current square we want to use to compare against
+        for pos in range(0,9):
+            if(curSeq[pos] == 0):
+                for arr in range(0,9):
+                    #call the find curcq func
+                    curSqNum = getSqNum(arr, pos)
+                    curSqSeq = curSqs[curSqNum]
+                    print("curSqNum = ", curSqNum)
+                    print("curSqSeq = ", curSqSeq)
+                    for i in range(0,9):
+                        if(curSqSeq[0][i] == 0):
+                            curSqSeq[0][i].remove(i)
+                    print("here are the numbers that it can't be from its curSq:", curSqSeq[curSqNum])
+        
+        print("Numbers that the positions cannot be")
+        print(noNoNums)
+        #return noNoNums
+
 
 #---------------------------------------------------------------------------------------------------------
 #use the dictionary to find the number that should go in place
 #find potential answers from the numbers that it can't be
 def potAns(badNums):
-    potNums = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
+    potNums = {}
     for i in range(0,9):
         for j in range(1,10):
             if(j not in badNums[i] and badNums[i] != []):
-                potNums[i].append(j)         
+                if(i not in potNums):
+                    potNums[i] = [j]
+                else:
+                    potNums[i].append(j)          
     print("potential answers")
     print(potNums)
 
 #---------------------------------------------------------------------------------------------------------    
 #solve the 3x3 
-def sudSolv(board, curSq):
+def sudSolv(board, curSeq):
     print("in sudSolv")
-    if(curSq == 10):
-        return board
     #get the middle of a 3x3 square
     #get the 3x3 of the first square to solve
     #center of square 1 is 1,1
     #create an array that will house the 9 numbers from a 3x3 
-    solvSeq = []
-    for j in range(0,3):
-        for k in range(0,3):
-            solvSeq.append(board[j][k])
-    print("cur solvSeq", solvSeq)
-    badnums = noNumDict(board, solvSeq)
-    potAns(badnums)
+
+    #for j in range(0,3):
+    #    for k in range(0,3):
+    #        solvSeq.append(board[j][k])
+    badnums = noNumDict(board)
+    #potAns(badnums)
     #increment square
-    curSq += 1
     #return solved board
     #UPDATE board TO SOLVED BOARD VARIABLE
     return board
@@ -145,7 +203,6 @@ SodBoard = [[0,1,0,5,0,9,0,7,0],
 print("initial board")
 for row in SodBoard:
     print(row)
-print("first 3x3")
 boardSolved = sudSolv(SodBoard, 0)
 print("Solved Board:")
 for row in boardSolved:
